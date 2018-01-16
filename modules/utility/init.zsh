3ee5519dfa7f05c128a9f9dc7ade4da26292259b
Salmanul Farzy <salmanulfarzy@gmail.com>
2018-01-16 17:25:38 +0530

    Disable lc alias in favour of `lc` command

--- modules/utility/init.zsh
+++ modules/utility/init.zsh
@@ -109,7 +109,6 @@ alias lm='la | "$PAGER"' # Lists human readable sizes, hidden files through page
 alias lx='ll -XB'        # Lists sorted by extension (GNU only).
 alias lk='ll -Sr'        # Lists sorted by size, largest last.
 alias lt='ll -tr'        # Lists sorted by date, most recent last.
-alias lc='lt -c'         # Lists sorted by date, most recent last, shows change time.
 alias lu='lt -u'         # Lists sorted by date, most recent last, shows access time.
 alias sl='ls'            # I often screw this up.
 
